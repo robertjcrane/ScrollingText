78ef31cebbec731fa8b2ce250db5269b7da7d047
robertjcrane <robert@robertcrane.net>
2019-01-22 17:05:00 +0000

    extra context menu added in lesson 04.3

--- app/src/main/java/net/robertcrane/scrollingtext/MainActivity.java
+++ app/src/main/java/net/robertcrane/scrollingtext/MainActivity.java
@@ -2,6 +2,12 @@ package net.robertcrane.scrollingtext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView article_text = findViewById(R.id.article);
+        registerForContextMenu(article_text);
+    }//end onCreate
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }//end onCreateContextMenu
+
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
-}
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_edit:
+                displayToast("Edit choice selected");
+                return true;
+            case R.id.context_share:
+                displayToast("Share choice selected");
+                return true;
+            case R.id.context_delete:
+                displayToast("delete choice selected");
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }//end switch
+    }//end onContextItemSelected
+    
+}//end class
